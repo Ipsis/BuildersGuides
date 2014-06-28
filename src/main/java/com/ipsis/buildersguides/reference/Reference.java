9d549f76af0ec0c1173d1b4534524d77a41040e2
Ipsis <ipsis418@gmail.com>
2014-06-28 17:26:02 +0100

    Base mode files

---
+++ src/main/java/com/ipsis/buildersguides/reference/Reference.java
@@ -0,0 +1,7 @@
+package ipsis.buildersguides.reference;
+
+public class Reference {
+
+    public static final String MOD_ID = "BuildersGuide";
+    public static final String MOD_NAME = "Builders Guide";
+}
