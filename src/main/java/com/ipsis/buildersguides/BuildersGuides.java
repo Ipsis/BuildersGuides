fc83ecb4c3e7f46e8ddb62a8ba0b8f23a8fd3eb6
Ipsis <ipsis418@gmail.com>
2014-07-29 12:49:48 +0100

    Link version to the gradle script. (Taken from EE3)

--- src/main/java/com/ipsis/buildersguides/BuildersGuides.java
+++ src/main/java/com/ipsis/buildersguides/BuildersGuides.java
@@ -10,10 +10,8 @@ import com.ipsis.buildersguides.block.BGBlocks;
 import com.ipsis.buildersguides.proxy.IProxy;
 import com.ipsis.buildersguides.reference.Reference;
 
-@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = BuildersGuides.VERSION)
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION)
 public class BuildersGuides {
-
-    public static final String VERSION = "1.7.10-0.1a";
 
     @Mod.Instance("BuildersGuides")
     public static BuildersGuides instance;
--- src/main/java/com/ipsis/buildersguides/reference/Reference.java
+++ src/main/java/com/ipsis/buildersguides/reference/Reference.java
@@ -4,6 +4,7 @@ public class Reference {
 
     public static final String MOD_ID = "BuildersGuides";
     public static final String MOD_NAME = "Builders Guides";
+    public static final String MOD_VERSION = "@VERSION@";
 
     public static final String CLIENT_PROXY_CLASS = "com.ipsis.buildersguides.proxy.ClientProxy";
     public static final String SERVER_PROXY_CLASS = "com.ipsis.buildersguides.proxy.ServerProxy";
