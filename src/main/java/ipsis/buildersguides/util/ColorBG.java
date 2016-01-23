d20f4e13aa2b0eb692dcd8ecd1d4a60036456efc
Ipsis <ipsis418@gmail.com>
2016-01-23 06:48:09 +0000

    Change ColorBG to take in the hex color value and convert it to the float rgb. Color codes taken from ColorHelper.java in CoFHLib

--- src/main/java/ipsis/buildersguides/util/ColorBG.java
+++ src/main/java/ipsis/buildersguides/util/ColorBG.java
@@ -1,22 +1,26 @@
 package ipsis.buildersguides.util;
 
 public enum ColorBG {
-    BLACK(0.0F, 0.0F, 0.0F),
-    RED(1.0F, 0.0F, 0.0F),
-    GREEN(0.0F, 1.0F, 0.0F),
-    BROWN(0.1F, 0.0F, 0.0F),
-    BLUE(0.0F, 0.0F, 1.0F),
-    PURPLE(1.0F, 0.0F, 1.0F),
-    CYAN(0.5F, 1.0F, 1.0F),
-    LIGHTGRAY(0.8F, 0.8F, 0.8F),
-    GRAY(1.0F, 1.0F, 1.0F),
-    PINK(1.0F, 1.0F, 1.0F),
-    LIME(1.0F, 1.0F, 1.0F),
-    YELLOW(1.0F, 1.0F, 0.0F),
-    LIGHTBLUE(0.0F, 0.5F, 1.0F),
-    MAGENTA(0.5F, 0.5F, 0.5F),
-    ORANGE(1.0F, 0.5F, 0.0F),
-    WHITE(1.0F, 1.0F, 1.0F);
+
+    /**
+     * Color code values taken from CoFHLib/ColorHelper.java
+     */
+    BLACK(0x191919),
+    RED(0xCC4C4C),
+    GREEN(0x667F33),
+    BROWN(0x7F664C),
+    BLUE(0x3366CC),
+    PURPLE(0xB266E5),
+    CYAN(0x4C9982),
+    LIGHTGRAY(0x999999),
+    GRAY(0x4C4C4C),
+    PINK(0xF2B2CC),
+    LIME(0x7FCC19),
+    YELLOW(0xE5E533),
+    LIGHTBLUE(0x99B2F2),
+    MAGENTA(0xE57FD8),
+    ORANGE(0xF2B233),
+    WHITE(0xFFFFFF);
 
     public static ColorBG[] VALID_COLORS = { BLACK, RED, GREEN, BROWN, BLUE, PURPLE, CYAN, LIGHTGRAY, GRAY, PINK, LIME, YELLOW, LIGHTBLUE, MAGENTA, ORANGE, WHITE };
 
@@ -28,15 +32,22 @@ public enum ColorBG {
         return ColorBG.WHITE;
     }
 
+    private int v;
     private float red;
     private float green;
     private float blue;
 
-    ColorBG(float red, float green, float blue) {
+    ColorBG(int v) {
 
-        this.red = red;
-        this.green = green;
-        this.blue = blue;
+        this.v = v;
+
+        this.red = ((v & 0xFF0000) >> 16) & 0xFF;
+        this.green = ((v & 0x00FF00) >> 8) & 0xFF;
+        this.blue = v & 0xFF;
+
+        this.red = this.red / 255.0F;
+        this.green = this.green / 255.0F;
+        this.blue = this.blue / 255.0F;
     }
 
     public float getRed() {
@@ -54,6 +65,11 @@ public enum ColorBG {
         return this.blue;
     }
 
+    public int getV() {
+
+        return this.v;
+    }
+
     public ColorBG getNext() {
 
         int next = ordinal();
@@ -68,6 +84,6 @@ public enum ColorBG {
     @Override
     public String toString() {
 
-        return "R:" + this.red + " G:" + this.getGreen() + " B:" + this.getBlue();
+        return super.toString() + " " + this.v + " R:" + this.red + " G:" + this.getGreen() + " B:" + this.getBlue();
     }
 }
