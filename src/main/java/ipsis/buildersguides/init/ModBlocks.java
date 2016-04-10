c9959518b4b9ce4328c46b376c8e4157b6147bdf
Ipsis <ipsis418@gmail.com>
2016-04-10 19:53:58 +0100

    Use GameRegistry.register

--- src/main/java/ipsis/buildersguides/init/ModBlocks.java
+++ src/main/java/ipsis/buildersguides/init/ModBlocks.java
@@ -2,14 +2,17 @@ package ipsis.buildersguides.init;
 
 import ipsis.buildersguides.block.BlockContainerBG;
 import ipsis.buildersguides.block.BlockMarker;
+import ipsis.buildersguides.reference.Reference;
 import ipsis.buildersguides.tileentity.TileEntityMarker;
+import net.minecraft.item.ItemBlock;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class ModBlocks {
 
     public static void init() {
 
-        GameRegistry.registerBlock(blockMarker, BlockMarker.BASENAME);
+        GameRegistry.register(blockMarker);
+        GameRegistry.register(new ItemBlock(blockMarker).setRegistryName(Reference.MOD_ID_LOWER, BlockMarker.BASENAME));
     }
 
     public static void registerTileEntities() {
