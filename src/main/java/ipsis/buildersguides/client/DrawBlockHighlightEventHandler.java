5f3aa2b922a3ec24dbddf65e9567bd0c6a979a11
Unknown <ipsis418@gmail.com>
2016-08-27 19:36:28 +0100

    Ignore non-BLOCK RayTraceResult [#13]
    Fixes the looking at ItemFrame while holding a marker issue

--- src/main/java/ipsis/buildersguides/client/DrawBlockHighlightEventHandler.java
+++ src/main/java/ipsis/buildersguides/client/DrawBlockHighlightEventHandler.java
@@ -8,6 +8,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
 import net.minecraftforge.client.event.DrawBlockHighlightEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -21,6 +22,9 @@ public class DrawBlockHighlightEventHandler {
 
         ItemStack held = event.getPlayer().getHeldItemMainhand();
         if (held == null || Item.getItemFromBlock(ModBlocks.blockMarker) != held.getItem())
+            return;
+
+        if (event.getTarget().typeOfHit != RayTraceResult.Type.BLOCK)
             return;
 
         EntityPlayer entityPlayer = event.getPlayer();
